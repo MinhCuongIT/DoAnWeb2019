5547b569402ff19064becb090197b65b02c6836c
1512351 <1512351@student.hcmus.edu>
2019-06-12 17:16:44 +0700

    Revert "add Profile admin page"
    This reverts commit 319e7f2d653b5ef25064f4c1f6aa93344a33c693, reversing
    changes made to 151524e1c568793c45c7be7d7e6212e7b44e2111.

--- middlewares/auth-local.mdw.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res, next) => {
-    if(req.user){
-        res.locals.isAuthenticated = true
-        res.locals.authUser = req.user
-    }
-    next()
-}
--- middlewares/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res, next)=>{
-
-    if(!req.user){
-        res.redirect('/account/login')
-    }
-    else{
-        next()
-    }
-}
